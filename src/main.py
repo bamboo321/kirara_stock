4d314635026d29fb478213edfbf1fa19975baf91
bamboo321 <28136882+bamboo321@users.noreply.github.com>
2021-11-07 09:42:00 +0000

    listからtupleに変更

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ def main():
 def output_csv(stock) -> None:
     with open('../data/' + stock.updated_time.strftime('%Y%m%d_%H:%M:%S') + '.csv', 'w') as f:
     # with open('a', 'w') as f:
-        header = ['商品番号', '商品カテゴリ', '商品名', '購入限定数', '在庫状況']
+        header = ('商品番号', '商品カテゴリ', '商品名', '購入限定数', '在庫状況')
         writer = csv.writer(f)
         writer.writerow(header)
         writer.writerows(stock.goods)
