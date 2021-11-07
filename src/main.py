47932de91adecd539ee99861bbf38a00bb18f85a
bamboo321 <28136882+bamboo321@users.noreply.github.com>
2021-11-07 09:36:59 +0000

    output_csvの引数変更

--- src/main.py
+++ src/main.py
@@ -5,16 +5,16 @@ import stock
 
 def main():
     s = stock.stock()
-    output_csv(s.updated_time, s.goods)
+    output_csv(s)
 
 
-def output_csv(updated_time, goods):
-    with open('../data/' + updated_time.strftime('%Y%m%d_%H:%M:%S') + '.csv', 'w') as f:
+def output_csv(stock) -> None:
+    with open('../data/' + stock.updated_time.strftime('%Y%m%d_%H:%M:%S') + '.csv', 'w') as f:
     # with open('a', 'w') as f:
         header = ['商品番号', '商品カテゴリ', '商品名', '購入限定数', '在庫状況']
         writer = csv.writer(f)
         writer.writerow(header)
-        writer.writerows(goods)
+        writer.writerows(stock.goods)
 
 
 if __name__ == '__main__':
